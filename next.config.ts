3c0943bb95cee406b6404dd21df0787567c57699
Ahmad Syarifuddin <syarifuddinahmads.me@gmail.com>
2025-05-21 15:19:03 +0700

    remove experimental

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    appDir: true,
-    viewTransitions: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
